54ecb82b3ef54b46e6ef4bec098b1b67beaf1b5c
Viktor Kleen <viktor@kleen.org>
2021-02-23 18:25:44 +0000

    move to a nix flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,16 @@
   description = "relayd and relayctl";
 
   inputs = {
-    nixpkgs.url = "github:vkleen/nixpkgs";
-
     haskell-nix.url = "github:input-output-hk/haskell.nix";
+    stackageSrc = {
+      url = "github:input-output-hk/stackage.nix";
+      flake = false;
+    };
+    hackageSrc = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
 
     spidev = {
       url = "github:vkleen/spidev";
@@ -13,57 +20,68 @@
   };
 
 
-  outputs = { self, nixpkgs, haskell-nix, ... }: let
+  outputs = inputs@{ self, nixpkgs, haskell-nix, ... }: let
     inherit (nixpkgs) lib;
-    pkgs' = system: (nixpkgs.legacyPackages."${system}").extend haskell-nix.overlay;
+    forAllSystems' = f: lib.mapAttrs f inputs.nixpkgs.legacyPackages;
+    forAllSystems = f: lib.mapAttrs f pkgs;
 
-    nonReinstallablePkgs = [
-      "Cabal"
-       "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
-      "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
-      # ghcjs custom packages
-      "ghcjs-prim" "ghcjs-th"
-      "ghc-boot"
-      "ghc" "Win32" "array" "binary" "bytestring" "containers"
-      "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
-      # "ghci" "haskeline"
-      "hpc"
-      "mtl" "parsec" "process" "text" "time" "transformers"
-      "unix" "xhtml"
-      "stm" "terminfo"
-    ];
+    pkgs = forAllSystems' (system: pkgs: pkgs.appendOverlays (
+      [ (haskell-nix.overlays.combined)
+        (final: prev: {
+          evalPackages = (import final.path {
+            overlays = [haskell-nix.overlays.combined-eval-on-build];
+            localSystem = system;
+          }).buildPackages;
+        })
+        (final: prev: {
+          haskell-nix = prev.haskell-nix // {
+            sources = prev.haskell-nix.sources // {
+              hackage = inputs.hackageSrc;
+              stackage = inputs.stackageSrc;
+            };
+          };
+        })
+      ]
+    ));
 
-    haskell = system: let
-      pkgs = pkgs' system;
-    in pkgs.haskell-nix.stackProject {
-      src = ./.;
-      sha256map = {
-        "https://github.com/vkleen/spidev.git"."a610703e7af18528b4d69389c5e04d0837b5160e" = "1ir9iqap987xkj4j1m938advngwj4qfxbyrv2d7bax07907wqsif";
-      };
-      pkg-def-extras = [
-      ];
-      stack-sha256 = "1wd4izz1ya7nf68r53pgm9hch891qsyb02099rx81c8dv101m3n7";
-      materialized = ./materialized;
-      modules = [ ({config, ...}: {
-        reinstallableLibGhc = true;
-        inherit nonReinstallablePkgs;
-        configureFlags = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isMusl [
-          "--ghc-option=-optl=-static"
-          "--ghc-option=-optl=-pthread"
-          "--extra-lib-dirs=${pkgs.gmp.override { withStatic = true; }}/lib"
-          "--extra-lib-dirs=${pkgs.zlib_both}/lib"
-          "--disable-executable-dynamic"
-          "--disable-shared"
-          "--enable-executable-stripping"
+    haskell = forAllSystems (system: p:
+      p.haskell-nix.project {
+        src = ./.;
+        sha256map = {
+          "https://github.com/vkleen/spidev.git"."6a2ad9cd12ae04903ae3806d3dc76a3e855ad7ef" = "1z5wyimz8ppxcfx40lzm9j3nl94vj67skhidxz52wkqhv9iflwbm";
+        };
+        pkg-def-extras = [
         ];
-      }) ];
-    };
+        compiler-nix-name = "ghc8104";
+        # materialized = ./materialized;
+      });
+
+    devShell = forAllSystems (system: p:
+      haskell.${system}.shellFor {
+        packages = ps: with ps; [
+          relayctl spidev
+        ];
+        tools = {
+          haskell-language-server = "latest";
+          cabal = "latest";
+          hpack = "latest";
+          c2hs = "latest";
+          ghcid = "latest";
+        };
+        buildInputs = [
+        ];
+
+        withHoogle = false;
+
+        exactDeps = true;
+      });
   in {
-    packages.x86_64-linux.relayctl = (haskell "x86_64-linux").relayctl.components.exes.relayctl;
-    packages.x86_64-linux.relayd = (haskell "x86_64-linux").relayctl.components.exes.relayd;
+    inherit devShell;
 
-    packages.aarch64-linux.relayd = (haskell "aarch64-linux").relayctl.components.exes.relayd;
+    packages.x86_64-linux.relayctl = haskell."x86_64-linux".hsPkgs.relayctl.components.exes.relayctl;
+    packages.x86_64-linux.relayd = haskell."x86_64-linux".hsPkgs.relayctl.components.exes.relayd;
 
-    materializedSha = (haskell "x86_64-linux").relayctl;
+    packages.aarch64-linux.relayd = haskell."aarch64-linux".hsPkgs.relayctl.components.exes.relayd;
+    packages.aarch64-linux.relayctl = haskell."aarch64-linux".hsPkgs.relayctl.components.exes.relayctl;
   };
 }
